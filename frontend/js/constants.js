70a177b1a8955d4ff5ec4dc6c671d5dd63d52d70
Fat Cat <akokulas23@gmail.com>
2022-03-25 23:51:20 +0800

    update js

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Matabang Pusa NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'Matabang Pusa is an NFT collection of 10,000 randomly generated cats stored as ERC-1155 tokens on the Polygon blockchain Breed from over 500 of the fattest cats in the entire world of Cat Condo, this side of each Fat Cat from curiosities like wearing weird clothes, scrap metals, and even a monster parts, along with special and rare combinations Fat Cats evolved in a new cat species like Meowster, Summercatto, Cybercatto and more. In this NFT project, we will share 20% of our earnings by adopting and feeding homeless cats here in the Philippines.';
+const welcome_p = 'The Matabang Pusa NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
